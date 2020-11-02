022a9194f51eb2f42a13adf9dd44ebad492c9b51
sjh15 <steven.handy@queensu.ca>
2020-11-02 09:41:56 -0500

    Add Docker version of analyze_segment

---
+++ python_functions/analyze_segment-docker/analyze_segment.py
@@ -0,0 +1,12 @@
+import ibm_fn_helper as ifh
+
+import numpy as np
+
+
+# Run analysis algorithms on this raw data segment (represented as a NumPy array)
+def analyze_segment(raw_data):
+
+    #### TODO: Define analysis algorithms here.  Return type is assumed to be a NumPy array.
+    analyzed_data = raw_data
+
+    return analyzed_data
